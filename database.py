9a823dccf2f7ce9bb207a7efdfb5b91446eb450d
infiniteloop <aksagg@iitk.ac.in>
2016-02-01 20:24:28 +0530

    added code for resetting database

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 import datetime
+import sys, os
 
 db = SqliteExtDatabase('my_database.db')
 
@@ -12,9 +13,17 @@ class User(BaseModel):
 	username = CharField(unique=True)
 	ip = CharField()
 
+class Channel(BaseModel):
+	name = CharField(unique=True)
+
 db.connect()
 print 'Database Connected'
 
-#Put if __main__ here
-def db_reset():
-	db.create_tables([User])
+if __name__ == "__main__":
+	if len(sys.argv) >=2 and sys.argv[1] == 'clear':
+		db.close()
+		os.remove('my_database.db')
+		db.connect()
+		db.create_tables([User])
+		db.close()
+		print 'Database cleared'
