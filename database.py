fa869c87c7004fbb725d54e2ac0d4f362a84de7e
infiniteloop <aksagg@iitk.ac.in>
2016-02-01 20:44:09 +0530

    added tables in db

--- database.py
+++ database.py
@@ -15,6 +15,22 @@ class User(BaseModel):
 
 class Channel(BaseModel):
 	name = CharField(unique=True)
+	num = IntegerField(default=0)
+
+class ChannelPublisher(BaseModel):
+	ch_id = ForeignKeyField(Channel, related_name='channel_pub')
+	pub_id = ForeignKeyField(User, related_name='publisher')
+
+class ChannelSubscriber(BaseModel):
+	ch_id = ForeignKeyField(Channel, related_name='channel_sub')
+	sub_id = ForeignKeyField(User, related_name='subscriber')
+	ts = IntegerField()
+
+class Feed(BaseModel):
+	ch_id = ForeignKeyField(Channel, related_name='channel_feed')
+	pub_id = ForeignKeyField(User, related_name='publisher_feed')
+	text = CharField()
+	sid = IntegerField()
 
 db.connect()
 print 'Database Connected'
@@ -24,6 +40,6 @@ if __name__ == "__main__":
 		db.close()
 		os.remove('my_database.db')
 		db.connect()
-		db.create_tables([User])
+		db.create_tables([User, Channel, ChannelPublisher, ChannelSubscriber, Feed])
 		db.close()
 		print 'Database cleared'
